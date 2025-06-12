45fc520a923f5dd689632318de7b3f27472c2fdd
Pedraza Vázquez, Francisco <fpedrazav02@gmail.com>
2025-06-12 23:36:05 +0200

    feat(Mods): Add incline and surround

--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -17,10 +17,58 @@ return {
     {
         'voldikss/vim-floaterm',
     },
+    -- UndoTree
     {
         'mbbill/undotree',
         config = function()
             vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
         end
-    }
+    },
+    -- Surround
+    {
+        "echasnovski/mini.surround",
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            custom_surroundings = nil,
+
+            highlight_duration = 300,
+
+            mappings = {
+                add = 'sa',            -- Add surrounding in Normal and Visual modes
+                delete = 'ds',         -- Delete surrounding
+                find = 'sf',           -- Find surrounding (to the right)
+                find_left = 'sF',      -- Find surrounding (to the left)
+                highlight = 'sh',      -- Highlight surrounding
+                replace = 'sr',        -- Replace surrounding
+                update_n_lines = 'sn', -- Update `n_lines`
+
+                suffix_last = 'l',     -- Suffix to search with "prev" method
+                suffix_next = 'n',     -- Suffix to search with "next" method
+            },
+
+            n_lines = 20,
+            respect_selection_type = false,
+            search_method = 'cover',
+            silent = false,
+        },
+    },
+    -- Get rid of whitespace
+    {
+        "echasnovski/mini.trailspace",
+        event = { "BufReadPost", "BufNewFile" },
+        config = function()
+            local miniTrailspace = require("mini.trailspace")
+
+            miniTrailspace.setup({
+                only_in_normal_buffers = true,
+            })
+            vim.keymap.set("n", "<leader>cw", function() miniTrailspace.trim() end, { desc = "Erase Whitespace" })
+            vim.api.nvim_create_autocmd("CursorMoved", {
+                pattern = "*",
+                callback = function()
+                    require("mini.trailspace").unhighlight()
+                end,
+            })
+        end,
+    },
 }
