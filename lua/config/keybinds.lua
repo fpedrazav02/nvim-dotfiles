389aa1da9e5b82b96e08ecb499028c2ef7e9cd9a
Francisco Pedraza Vázquez <fpedrazav02@gmail.com>
2025-06-17 11:58:18 +0200

    Update keybinds.lua

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -7,3 +7,19 @@ vim.keymap.set("n", "<leader>nh", vim.cmd.noh)
 vim.keymap.set("n", "<leader>kk", function()
     vim.lsp.buf.format({ async = true })
 end, { desc = "Format file" })
+
+vim.g.clipboard = {
+  name = 'osc52',
+  copy = {
+    ["+"] = require('vim.ui.clipboard.osc52').copy,
+    ["*"] = require('vim.ui.clipboard.osc52').copy,
+  },
+  paste = {
+    ["+"] = require('vim.ui.clipboard.osc52').paste,
+    ["*"] = require('vim.ui.clipboard.osc52').paste,
+  },
+}
+vim.g.oscyank_term = 'default'
+vim.keymap.set('n', '<leader>c', '<Plug>OSCYankOperator', {silent = true})
+vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true, silent = true})
+vim.keymap.set('v', '<leader>c', '<Plug>OSCYankVisual', {silent = true})
