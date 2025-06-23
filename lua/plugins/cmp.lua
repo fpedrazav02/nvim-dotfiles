033e283c6fa35e61280ac8b9f1b0d20ba71ba006
Francisco Pedraza Vázquez <fpedrazav02@gmail.com>
2025-06-23 10:46:51 +0200

    Update cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,23 +6,49 @@ return {
     "hrsh7th/cmp-path",
     "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
   },
-  opts = function()
+  config = function()
     local cmp = require("cmp")
-    return {
-      preselect = cmp.PreselectMode.None,
-      mapping = {
-        ["<Tab>"]   = cmp.mapping.select_next_item(),
-        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-        ["<CR>"]    = cmp.mapping.confirm({ select = false }),
+    local luasnip = require("luasnip")
+
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
       },
-      sources = {
+      preselect = cmp.PreselectMode.None,
+      mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+      }),
+      sources = cmp.config.sources({
         { name = "nvim_lsp" },
+        { name = "luasnip" },
         { name = "buffer" },
         { name = "path" },
-        { name = "luasnip" },
-      },
-    }
+      }),
+    })
   end,
 }
-
