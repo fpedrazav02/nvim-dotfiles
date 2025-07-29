29abc8071f93960657abea91604ba98212323db6
Francisco Pedraza Vázquez <fpedrazav02@gmail.com>
2025-07-29 14:04:13 +0900

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,13 +3,23 @@ return {
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile" },
   config = function()
-    require("nvim-treesitter.configs").setup({
+    local ok, configs = pcall(require, "nvim-treesitter.configs")
+    if not ok then
+      vim.notify("nvim-treesitter no se pudo cargar", vim.log.levels.ERROR)
+      return
+    end
+
+    configs.setup({
       ensure_installed = {
-        "lua", "python", "json", "yaml", "bash", "html", "css", "javascript", "c","cpp"
+        "lua", "python", "json", "yaml", "bash", "html", "css", "javascript", "c", "cpp"
       },
       auto_install = true,
       highlight = {
         enable = true,
+        disable = function(lang, buf)
+          local ok = pcall(vim.treesitter.get_parser, buf, lang)
+          return not ok
+        end,
         additional_vim_regex_highlighting = false,
       },
       indent = {
