feffaf9a7e20db09a2a34612c8dfcb13d1624bcd
Francisco Pedraza Vázquez <fpedrazav02@gmail.com>
2025-08-19 13:52:43 +0200

    Update colors.lua

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,8 +1,9 @@
 return {
     {
-        "Mofiqul/vscode.nvim",
+        "projekt0n/github-nvim-theme",
         config = function()
-            vim.cmd.colorscheme "vscode"
+            vim.o.background = "dark"
+            vim.cmd.colorscheme "github_dark_default"
         end
     },
 }
